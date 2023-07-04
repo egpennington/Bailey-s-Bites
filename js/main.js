29c967cee3079e69066785e8212f5a714342c97a
egpennington <egpennington@hotmail.com>
2023-07-04 16:08:16 -0500

    Upload first time
    Real time movie data base

---
+++ js/main.js
@@ -0,0 +1,65 @@
+import { initializeApp } from https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js
+import { getDatabase, ref, push, onValue, remove } from https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js
+
+const appSettings = {
+  databaseURL: https://bailey-s-bites-default-rtdb.firebaseio.com/
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database, "movies")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const movieListEl = document.getElementById("movie-list")
+
+onValue(moviesInDB, function(snapshot) {
+  let moviesArray = Object.entries(snapshot.val())
+  
+   moviesArray.reverse()
+  
+  clearMovieListEl()
+  
+  for (let i = 0; i < moviesArray.length; i++) {
+    let currentMovie = moviesArray[i]   
+    let currentMovieID = currentMovie[0]
+    let currentMovieValue = currentMovie[1]
+    
+    appendMovieListEl(currentMovie)
+  }
+
+})
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    clearMovieListEl()
+    push(moviesInDB, inputValue)
+    clearInput()
+})
+
+function clearInput() {
+   inputFieldEl.value = ""
+}
+
+function clearMovieListEl() {
+    movieListEl.innerHTML = ""
+}
+
+function appendMovieListEl(movie) {
+   /* movieListEl.innerHTML += ` <li>${listItem}</li>` */
+   
+   let movieID = movie[0]
+   let movieValue = movie[1]
+   
+   let newEl = document.createElement("li")
+   
+   newEl.textContent = movieValue
+   
+   newEl.addEventListener("click", function() {
+       let exactLocationOfItemInDB = ref(database, `movies/${movieID}`)
+       
+       remove(exactLocationOfItemInDB)
+   })
+   
+   movieListEl.append(newEl)
+}
